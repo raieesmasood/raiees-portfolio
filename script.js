5d449036295e6ee7d5228faddad204ca21a660d9
raieesmasood <raieesmasoodi@gmail.com>
2024-08-18 23:53:14 +0530

    v-1.0.7

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// reloading but the path stays same
+// window.addEventListener('load', function() {
+//   // Scroll to the top of the page
+//   window.scrollTo(0,0).scrollIntoView({behaviour: 'smooth'});
+//   document.getElementById("hero-profile").scrollIntoView({behaviour: 'smooth'});
+// });
+
+window.addEventListener('load', () => {
+  // Remove the hash from the URL
+  if (window.location.hash) {
+      history.replaceState(null, null, window.location.pathname);
+  }
+  // Scroll to the top of the page with smooth scrolling
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
